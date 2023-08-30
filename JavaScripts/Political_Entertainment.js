4a67712e572f44308f734b78fc99b5e63b4fd721
Heinrich Worring <easySoft-cooperation@web.de>
2023-08-30 09:06:33 -0400

    Update Political_Entertainment.js

--- JavaScripts/Political_Entertainment.js
+++ JavaScripts/Political_Entertainment.js
@@ -9,7 +9,7 @@
    var politicalImg = "vonAnfang_an_eine_Lüge, AerzteInterview, eine_neue_Zeit, da_sprach_Gott, die_Etablierten, wir_können_noch_ganz_anders, UmzugsUnternehmen, UnternehmensBeratung, "+
                        "Merkel-and-Trump, Trump-Queen, Trump-Amerika_first, Covid-19_German-solution"; 
    var splitedImg = politicalImg.split(", ");  
-   var werbungImg = "Apollo_Optik, DentaBlink, DentalKlinik, Die_Milch_machts, Fremdenverkehrsamt_Tirol, Frosta, HairDressing, LieferHeld, Mon_Cheri, PartnerShip, Werkzeuge"; 
+   var werbungImg = "Apollo_Optik, DentaBlink, DentalKlinik, Die_Milch_machts, Fremdenverkehrsamt_Tirol, Frosta, HairDressing, LieferHeld, Mon_Cheri, PartnerShip, Werkzeuge, BlueJean"; 
    var splitedWerbung = werbungImg.split(", ");
    var thema = "PolitischMotiviert, " +
                "Werbung";
